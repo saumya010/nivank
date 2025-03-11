99772b3ee0f08d8b90f42ffa58f77f0b604708b2
Saumya Sharma <saumya@mac.home>
2025-03-11 17:04:36 -0400

    added faq section

---
+++ app/components/FAQ.jsx
@@ -0,0 +1,229 @@
+import React from "react";
+
+const FAQ = () => {
+  return (
+    <section className="py-14 pt-10 lg:pb-[138px] code-section" id="sgkpo4js">
+      <div className="mx-auto max-w-[1570px] px-5 lg:px-10 2xl:px-5">
+        <h2 className="mb-4 text-center text-[35px] font-medium leading-normal text-[var(--dark-text-color)] sm:mb-10 sm:text-4xl sm:leading-[78px] lg:text-[60px]">
+          Frequently
+          <span className="text-[var(--primary-color)]">Asked Questions</span>
+        </h2>
+        <div className="mx-auto max-w-[1280px]" data-landingsite-faq="">
+          <div
+            className="mb-2.5 rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:mb-[18px] sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px] sm:shadow-[0_35px_90px_0px_rgba(76,80,113,0.73)]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                How Do I Start My Custom Design Journey with Nivank?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:text-lg lg:leading-[30px]">
+                Starting your custom design process with Nivank is simple! Reach
+                out through our{" "}
+                <a href="/services/custom-design">Custom Design Services</a>{" "}
+                page, where you can fill out a form detailing your vision. Our
+                dedicated team will contact you to discuss your ideas and
+                preferences.
+              </p>
+            </div>
+          </div>
+          <div
+            className="mb-2.5 rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:mb-[18px] sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                What Fabrics Are Available for My Custom Outfit?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="g:text-lg text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:leading-[30px]">
+                We offer an extensive selection of premium fabrics, including
+                silks, chiffons, georgettes, and cotton, each available in
+                various colors and patterns. Our team will guide you through
+                choosing the perfect fabric that aligns with your style and
+                occasion.
+              </p>
+            </div>
+          </div>
+          <div
+            className="mb-2.5 rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:mb-[18px] sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                Can I Request Changes to My Design After Ordering?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="g:text-lg text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:leading-[30px]">
+                Absolutely! We understand that your vision may evolve. You can
+                request modifications to your design up until the finalization
+                of your order. Our goal is to ensure your outfit is exactly as
+                you envision it.
+              </p>
+            </div>
+          </div>
+          <div
+            className="mb-2.5 rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:mb-[18px] sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                What Is the Typical Turnaround Time for Custom Orders?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="g:text-lg text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:leading-[30px]">
+                The time required for custom orders may vary based on
+                complexity, but we strive to complete designs within 4-6 weeks.
+                Please consult with us for a more precise timeline based on your
+                specific requirements.
+              </p>
+            </div>
+          </div>
+          <div
+            className="mb-2.5 rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:mb-[18px] sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                Do You Provide Alterations After Delivery?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="g:text-lg text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:leading-[30px]">
+                Yes, we offer complimentary minor alterations post-delivery to
+                ensure a perfect fit. For significant changes, we can provide
+                recommendations for trusted local tailors.
+              </p>
+            </div>
+          </div>
+          <div
+            className="mb-2.5 rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:mb-[18px] sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                How Can I Ensure I Choose the Right Size?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="g:text-lg text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:leading-[30px]">
+                To guarantee a flawless fit, we provide a comprehensive sizing
+                guide on our website. Our team is also available for virtual
+                consultations if you need personalized assistance with
+                measurements.
+              </p>
+            </div>
+          </div>
+          <div
+            className="rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                Can I View Past Custom Designs for Inspiration?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="g:text-lg text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:leading-[30px]">
+                Absolutely! Visit our <a href="/gallery">Gallery</a> to explore
+                a collection of our past creations. This will give you a sense
+                of the styles and craftsmanship we offer, helping you to refine
+                your vision.
+              </p>
+            </div>
+          </div>
+          <div
+            className="rounded-[15px] border border-[var(--dark-border-color)] bg-[#FFFFFF] px-3.5 py-5 sm:pb-[29px] sm:pl-9 sm:pr-[31px] sm:pt-[26px]"
+            data-landingsite-faq-item=""
+          >
+            <button
+              type="button"
+              className="flex w-full items-center justify-between"
+              data-landingsite-faq-question=""
+            >
+              <p className="pr-5 text-left text-xl font-medium leading-[30px] text-[var(--dark-text-color)] sm:pr-0 sm:text-lg sm:leading-6 lg:text-[25px] lg:leading-[30px]">
+                What Should I Consider When Planning My Outfit for a Special
+                Event?
+              </p>
+              <i
+                className="fa-solid fa-chevron-down text-lg text-[var(--dark-text-color)]"
+                aria-hidden="true"
+              ></i>
+            </button>
+            <div className="mt-2.5 hidden" data-landingsite-faq-answer="">
+              <p className="g:text-lg text-base font-normal leading-[26px] text-[var(--dark-text-color)] sm:leading-6 lg:leading-[30px]">
+                Consider the event's theme, your comfort level, and the season
+                while planning your outfit. Our designers are here to help you
+                select a design that not only looks stunning but also feels
+                perfect for the occasion.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FAQ;
