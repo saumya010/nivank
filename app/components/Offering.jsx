3ac41c3ee03da8c2db3c6f8ecb90129471176f13
Saumya Sharma <saumya@mac.home>
2025-03-11 16:56:49 -0400

    added offering section

---
+++ app/components/Offering.jsx
@@ -0,0 +1,197 @@
+import React from "react";
+
+const Offering = () => {
+  return (
+    <section
+      className="relative z-10 my-10 lg:mb-[143px] code-section"
+      id="say6gkh"
+    >
+      <div className="mx-auto max-w-[1570px] sm:px-10 2xl:px-5">
+        <div className="rounded-[30px] bg-[#F5F6F9] px-5 pb-[100px] pt-9 sm:rounded-[50px] sm:p-6 lg:px-9 lg:pb-12 lg:pt-14">
+          <div className="flex flex-col items-center justify-between sm:flex-row">
+            <div className="flex flex-col items-center gap-1.5 sm:flex-row sm:gap-[30px]">
+              <div className="flex items-center justify-center gap-[11px] rounded-[60px] border border-[var(--dark-border-color)] px-5 text-lg font-medium text-[var(--dark-text-color)] sm:py-1.5 sm:text-base xl:py-2.5 xl:text-lg">
+                Our Offerings
+              </div>
+              <h4 className="text-[30px] font-semibold text-[var(--dark-text-color)] sm:text-[22px] md:text-[30px]">
+                Custom-Made Elegance
+              </h4>
+            </div>
+            <a
+              href="/products"
+              className="absolute bottom-10 left-1/2 inline-flex h-[50px] -translate-x-1/2 items-center justify-center gap-3 rounded-[60px] bg-[var(--primary-color)] p-[5px] pl-6 text-lg font-medium text-[var(--primary-button-text-color)] sm:static sm:translate-x-0"
+            >
+              Explore Our Collection
+              <span className="flex h-10 w-10 items-center justify-center rounded-full bg-[var(--primary-button-text-color)]">
+                <i
+                  className="fa-solid fa-arrow-up rotate-45 text-lg text-[var(--primary-color)]"
+                  aria-hidden="true"
+                ></i>
+              </span>
+            </a>
+          </div>
+          <div className="mb-10 mt-5 overflow-hidden sm:my-[30px] md:mb-12 md:mt-10">
+            <div className="flex gap-[15px] transition-transform duration-500 ease-in-out sm:gap-3 lg:gap-4">
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/2b1ccaf5-bd4c-40cd-f9c4-564b66b77f00/public"
+                  alt=""
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Exquisite Lehengas
+                  </h6>
+                </div>
+              </div>
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/8319244d-7f14-467e-d40c-58786df60600/publicContain"
+                  alt="Elegant Traditional Saree"
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Graceful Sarees
+                  </h6>
+                </div>
+              </div>
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/d64c8abb-bea2-4448-459d-ff0434d4b200/public"
+                  alt=""
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Chic Indo-Western
+                  </h6>
+                </div>
+              </div>
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/1963497f-bcbc-493c-bd5b-eb859002d500/publicContain"
+                  alt="Elegant Lucknowi Chikankari Kurti"
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Lucknowi Chikankari
+                  </h6>
+                </div>
+              </div>
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/a7630554-7781-483a-1789-3bb2dbba1f00/public"
+                  alt=""
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Sophisticated Office Wear
+                  </h6>
+                </div>
+              </div>
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/4494bcb9-539c-4b29-05c4-e9d654309e00/public"
+                  alt=""
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Elegant Custom Designs
+                  </h6>
+                </div>
+              </div>
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/37a49399-ce05-401d-0f23-aa8671964400/publicContain"
+                  alt=""
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Beautiful Festive Wear
+                  </h6>
+                </div>
+              </div>
+              <div
+                className="relative h-[448px] w-[350px] flex-shrink-0"
+                data-landingsite-carousel-item=""
+              >
+                <img
+                  src="https://imagedelivery.net/xaKlCos5cTg_1RWzIu_h-A/b7526f87-1fcc-42b7-86a3-11297cf61c00/public"
+                  alt=""
+                  className="h-full w-full rounded-[35px] object-cover"
+                />
+                <div className="absolute bottom-5 left-0 w-full text-center xl:bottom-10">
+                  <h6 className="text-lg font-medium text-[var(--light-text-color)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] lg:text-2xl">
+                    Spa-Like Attire
+                  </h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-3 hidden items-center justify-between gap-5 sm:flex md:mt-5 md:gap-10">
+            <ul className="flex items-center gap-5">
+              <li className="">
+                <button
+                  type="button"
+                  className="flex h-[50px] w-[50px] items-center justify-center rounded-full border border-[var(--primary-color)]"
+                  data-landingsite-carousel-controls-left=""
+                >
+                  <i
+                    className="fa-solid fa-arrow-left text-lg text-[var(--primary-color)]"
+                    aria-hidden="true"
+                  ></i>
+                </button>
+              </li>
+              <li className="">
+                <button
+                  type="button"
+                  className="flex h-[50px] w-[50px] items-center justify-center rounded-full border border-[var(--primary-color)] bg-[var(--primary-color)]"
+                  data-landingsite-carousel-controls-right=""
+                >
+                  <i
+                    className="fa-solid fa-arrow-right text-lg text-[var(--primary-button-text-color)]"
+                    aria-hidden="true"
+                  ></i>
+                </button>
+              </li>
+            </ul>
+            <p className="ml-auto max-w-[760px] text-end text-sm font-normal leading-[22px] text-[var(--dark-text-color)] md:text-lg md:leading-[30px]">
+              Discover the sophistication of bespoke attire with Nivank, where
+              each creation merges timeless tradition with modern
+              elegance—crafted just for you for every occasion.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="absolute left-0 right-0 top-1/2 -z-10 mx-auto h-[103%] w-[95%] max-w-full -translate-y-1/2 rounded-[30px] bg-[var(--primary-color)] sm:h-[90%] sm:w-full sm:max-w-[590px] sm:rounded-[50px] md:max-w-[750px] lg:max-w-[980px] xl:max-w-[1590px]"></div>
+    </section>
+  );
+};
+
+export default Offering;
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Offering from "./components/Offering";
 import Transform from "./components/Transform";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
     <Hero />
     <About />
     <Transform />
+    <Offering />
     <Footer />
     </>
   );
